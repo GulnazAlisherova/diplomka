37ee8cf498d213f1a7ed0add0eb17b80fc017985
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-04-07 20:21:10 +0600

    fixed a bug: chanhed h2 tags to pages with several imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import NotFound from "./pages/NotFound";
+import Contacts from "./pages/Contacts";
+import Delivery from "./pages/Delivery";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -10,11 +13,10 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/contacts" element={<h1>Contacts</h1>} />
-          <Route path="/delivery" element={<h1>Delivery</h1>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/delivery" element={<Delivery />} />
           <Route path="/categories/:slug" element={<Category />} />
-
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
