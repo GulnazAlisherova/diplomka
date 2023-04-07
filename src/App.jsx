590c41b9fe768cde3fa54f96605fe687c058c01e
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-04-07 19:55:24 +0600

    fixed a bug added new many changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-export default function App(){
-  return(
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contacts" element={<Contacts/>}/>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contacts" element={<h1>Contacts</h1>} />
+          <Route path="/delivery" element={<h1>Delivery</h1>} />
+          <Route path="/categories/:slug" element={<Category />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </div>
-  )
+  );
 }
+
+export default App;
