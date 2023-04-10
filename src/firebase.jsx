bc7f6aeff2f7b5a83e16bd0aa727d937e68a72d2
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-04-10 14:47:05 +0600

    inside folder src created new file "firebase.jsx" and pasted a copy of main

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9PQdaXHb7-Fj9cm2Q-Rm4JIVhhRsmeIQ",
+  authDomain: "elet-company.firebaseapp.com",
+  projectId: "elet-company",
+  storageBucket: "elet-company.appspot.com",
+  messagingSenderId: "862703181912",
+  appId: "1:862703181912:web:51e630f1668a59ccc36a2d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
