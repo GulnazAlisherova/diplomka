cf7aba82af3aa30065b8d430cb3809dc1a6a31ad
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-04-14 14:43:47 +0600

    found many bugs on file "App.jsx" and fixed them

--- src/App.jsx
+++ src/App.jsx
@@ -7,57 +7,48 @@ import Contacts from "./pages/Contacts";
 import Delivery from "./pages/Delivery";
 import About from "./pages/About";
 import { createContext, useEffect, useState } from "react";
-import { getDocs } from "firebase/firestore";
-import { categoryCollection, productCollection } from "./firebase";
+import { getDocs } from "firebase/firestore/lite";
+import { categoryCollection, productsCollection } from "./firebase";
 
-
+// Создать контекст, который будет хранить данные.
 export const AppContext = createContext({
   categories: [],
-  products: []
+  products: [],
 });
 
 export default function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
 
-  // выполнить эту функцию только один раз
   useEffect(() => {
-    // получить категории из списка категорий
-    getDocs(categoryCollection)
-      .then(snapshot => {
-        // категории будут храниться в snapshot.docs
+    // выполнить только однажды
+    getDocs(categoryCollection) // получить категории
+      .then(({ docs }) => {
+        // когда категории загрузились
+        setCategories(
+          // обновить состояние
+          docs.map((doc) => ({
+            // новый массив
+            ...doc.data(), // из свойств name, slug
+            id: doc.id, // и свойства id
+          }))
+        );
+      });
 
-        // создать массив для категорий
-        const newCategories = [];
-        // заполнить массив данными из списка категорий
-        snapshot.docs.forEach(doc => { // doc = категория
-          const category = doc.data();
-          category.id = doc.id;
-
-          newCategories.push(category);
-        });
-        // задать новый массив как состояние комапо
-        setCategories(newCategories);
-      })
-
-    // получить продукты из списка продуктов
-    getDocs(productCollection)
-      .then(snapshot => {
-        // продукты будут храниться в snapshot.docs
-
-        // создать массив для продуктов
-        const newProducts = [];
-        // заполнить массив данными из списка продвук
-        snapshot.docs.forEach(doc => { // doc = продукт
-          const product = doc.data();
-          product.id = doc.id;
-
-          newProducts.push(product);
-        });
-        // задать новый массив как состояние комапо
-        setProducts(newProducts);
-      })
+    getDocs(productsCollection) // получить категории
+      .then(({ docs }) => {
+        // когда категории загрузились
+        setProducts(
+          // обновить состояние
+          docs.map((doc) => ({
+            // новый массив
+            ...doc.data(), // из свойств name, slug
+            id: doc.id, // и свойства id
+          }))
+        );
+      });
   }, []);
+
   return (
     <div className="App">
       <AppContext.Provider value={{ categories, products }}>
@@ -75,4 +66,3 @@ export default function App() {
     </div>
   );
 }
-
