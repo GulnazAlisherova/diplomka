5a3e0af662997af5e36e2e7bbf120103eba350f7
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-04-17 13:52:17 +0600

    inside file ProductList were added many changes -> products

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,18 +1,26 @@
 import { useContext } from "react";
-import "./ProductList.css";
 import { AppContext } from "../../App";
-import { Link } from "react-router-dom";
+import "./ProductList.css";
+import { NavLink } from "react-router-dom";
 
-export default function ProductList() {
+export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
-  const output = products.map((product) => (
-    <div className="Product" key={product.id}>
-      <img src={product.picture} alt={product.name} />
-      <Link to={"/product/" + product.path}>{product.name}</Link>
-      <span>{product.price} som</span>
-    </div>
-  ));
+  const output = products
+    .filter(product => product.category === category.id)
+    .map(product => (
+      <div key={product.id} className="Product">
+        <img src={product.picture} alt={product.name} />
+        <NavLink to={'/products/' + product.slug}>
+          {product.name}
+        </NavLink>
+        <span>{product.price} som</span>
+      </div>
+    ));
 
-  return <div className="ProductList">{output}</div>;
+  return (
+    <div className="ProductList">
+      {output}
+    </div>
+  );
 }
