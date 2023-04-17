bd6dd9a6796098a841aa8bf0de65a382580b452c
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-04-17 13:51:29 +0600

    added new tag for file CategoryList" and fixed some bugs

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-
 import { useContext } from "react";
 import { AppContext } from "../../App";
 
@@ -8,11 +7,13 @@ export default function CategoryList() {
 
   const output = categories.map((category) => (
     <li key={category.id}>
-      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
+      <NavLink to={`/categories/${category.slug}`}>{category.name}</NavLink>
     </li>
   ));
+
   return (
     <div className="CategoryList">
+      <h3>Categories</h3>
       <ul>{output}</ul>
     </div>
   );
