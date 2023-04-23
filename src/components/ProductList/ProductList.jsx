5797d4e2ca5237547dcfd469e9990540a789fd2a
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-04-23 16:02:37 +0600

    fixed a bug with using -> ctrl+h from "path" to "slug"  inside file ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,26 +1,22 @@
 import { useContext } from "react";
-import { AppContext } from "../../App";
 import "./ProductList.css";
-import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+import AddToCart from "../AddToCart/AddToCart";
 
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
   const output = products
-    .filter(product => product.category === category.id)
-    .map(product => (
-      <div key={product.id} className="Product">
+    .filter((product) => product.category === category.id)
+    .map((product) => (
+      <div className="Product" key={product.id}>
         <img src={product.picture} alt={product.name} />
-        <NavLink to={'/products/' + product.slug}>
-          {product.name}
-        </NavLink>
+        <Link to={"/product/" + product.slug}>{product.name}</Link>
         <span>{product.price} som</span>
+        <AddToCart product={product} />
       </div>
     ));
 
-  return (
-    <div className="ProductList">
-      {output}
-    </div>
-  );
+  return <div className="ProductList">{output}</div>;
 }
