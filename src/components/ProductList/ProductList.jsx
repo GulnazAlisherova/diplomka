e61f13db23ab3350f07a3b3f5a70f00d47a6b75c
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-05-03 06:49:57 +0600

    inside file "ProductList"  added "AddProduct" with import and many other changes

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,22 +1,31 @@
 import { useContext } from "react";
-import "./ProductList.css";
 import { AppContext } from "../../App";
-import { Link } from "react-router-dom";
+import "./ProductList.css";
+import { NavLink } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
+import AddProduct from "../AddProduct/AddProduct";
 
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
   const output = products
-    .filter((product) => product.category === category.id)
-    .map((product) => (
-      <div className="Product" key={product.id}>
+    .filter(product => product.category === category.id)
+    .map(product => (
+      <div key={product.id} className="Product">
         <img src={product.picture} alt={product.name} />
-        <Link to={"/product/" + product.slug}>{product.name}</Link>
+        <NavLink to={'/products/' + product.slug}>
+          {product.name}
+        </NavLink>
         <span>{product.price} som</span>
         <AddToCart product={product} />
       </div>
     ));
 
-  return <div className="ProductList">{output}</div>;
+  return (
+    <div className="ProductList">
+      {output}
+
+      <AddProduct />
+    </div>
+  );
 }
