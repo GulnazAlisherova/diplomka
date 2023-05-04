8c220d01a3f11b1bddbbc42f5ca475ded98990ff
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-05-04 06:22:48 +0600

    formated file "firebase.jsx" and fixed some bugs

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,14 +1,15 @@
 // Import the functions you need from the SDKs you need
-
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from 'firebase/firestore/lite';
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
 import {
   GoogleAuthProvider,
   getAuth,
   onAuthStateChanged,
   signInWithPopup,
   signOut,
-} from 'firebase/auth';
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,7 +24,7 @@ const firebaseConfig = {
 // Инициализация приложения
 const app = initializeApp(firebaseConfig);
 // Инициализация базы данных
-const db = getFirestore(app);
+export const db = getFirestore(app);
 const auth = getAuth(app);
 
 // Получение списка категорий (коллекции документов)
@@ -35,3 +36,31 @@ const provider = new GoogleAuthProvider();
 export const logIn = () => signInWithPopup(auth, provider);
 export const logOut = () => signOut(auth);
 export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
+
+export const onCategoriesLoad = (callback) =>
+  onSnapshot(categoryCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onProductsLoad = (callback) =>
+  onSnapshot(productsCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onOrdersLoad = (callback) =>
+  onSnapshot(ordersCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
