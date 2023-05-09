59f3314f98eb556a28c6c995e1fb8ad7420e2689
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-05-09 18:26:23 +0600

    added tag p with lorem inside

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -3,6 +3,7 @@ export default function Contacts() {
     <div className="Contacts">
       <h1>Contacts</h1>
       <p>Here our contacts</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, iste!</p>
       <h2>+996 553 921 876</h2>
     </div>
   );
