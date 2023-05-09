871850c1325de3c3124463d9de80c93f1e1da01d
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-05-09 18:24:14 +0600

    added test lorem for file Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="Home">
       <h1>Home</h1>
       <p>This is my home page</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quam? Sunt provident ullam, exercitationem ad nobis voluptatum praesentium velit magni consequuntur inventore vitae corporis et obcaecati sit? Blanditiis, neque! Pariatur.</p>
     </div>
   );
 }
