a23d51b702d6d17718ab9f7983652e51791bb3b9
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-05-09 18:27:25 +0600

    added tag p with lorem inside: inside file "About.jsx"

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ export default function About() {
     <div className="About">
       <h1>This is my About page</h1>
       <p>Here we tell you about our company</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto veniam laudantium quaerat corporis optio, dignissimos placeat magnam omnis commodi asperiores reiciendis nemo officiis. Debitis, repellat. Ex possimus sequi aperiam ad.</p>
     </div>
   );
 }
