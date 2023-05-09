d3e255179f97e8bc19f72a004c322fe73a66b60c
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-05-09 18:07:05 +0600

    inside file "ProductList.jsx"  was added many changes

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
-import { AppContext } from "../../App";
 import "./ProductList.css";
-import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
 
@@ -9,22 +9,21 @@ export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
   const output = products
-    .filter(product => product.category === category.id)
-    .map(product => (
-      <div key={product.id} className="Product">
-        <img src={product.picture} alt={product.name} />
-        <NavLink to={'/products/' + product.slug}>
-          {product.name}
-        </NavLink>
-        <span>{product.price} som</span>
-        <AddToCart product={product} />
-      </div>
-    ));
+  .filter(product => product.category === category.id)
+  .map(product => (
+    <div className="Product" key={product.id}>
+      <img src={product.picture} alt={product.name} />
+      <Link to={"/product/" + product.slug}>
+        {product.name}
+      </Link>
+      <span>{product.price} som</span>
+      <AddToCart product={product} />
+    </div>
+  ));
 
   return (
     <div className="ProductList">
       {output}
-
       <AddProduct />
     </div>
   );
