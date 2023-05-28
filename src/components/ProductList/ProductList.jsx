9fdb6d3dec62f26921ea6f776aba2e42b0ab9bb4
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-05-28 10:49:31 +0600

    files: App.jsx and ProductList.jsx were formated by my hands

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -9,18 +9,16 @@ export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
   const output = products
-  .filter(product => product.category === category.id)
-  .map(product => (
-    <div className="Product" key={product.id}>
-      <img src={product.picture} alt={product.name} />
-      <Link to={"/product/" + product.slug}>
-        {product.name}
-      </Link>
-      <span>{product.price} som</span>
-      {/* <p> {product.temp}</p> */}
-      <AddToCart product={product} />
-    </div>
-  ));
+    .filter((product) => product.category === category.id)
+    .map((product) => (
+      <div className="Product" key={product.id}>
+        <img src={product.picture} alt={product.name} />
+        <Link to={"/product/" + product.slug}>{product.name}</Link>
+        <span>{product.price} som</span>
+        {/* <p> {product.temp}</p> */}
+        <AddToCart product={product} />
+      </div>
+    ));
 
   return (
     <div className="ProductList">
