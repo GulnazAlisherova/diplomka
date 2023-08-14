31b4b2c18b1ea76e9d6d21c2bd088ed0f922999e
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-08-14 09:43:16 +0600

    added image for file NotFound, if there is smth wrong on page or another situation there is will appear a text and "error" image

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,10 @@
+import error from "../assets/error.png"
 export default function NotFound() {
   return (
     <div className="NotFound">
       <h1>404</h1>
       <h2>Everyone left to eat ashlyam-fu.</h2>
+      <img src={error}  alt="error" className="error" />
     </div>
   );
 }
